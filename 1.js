19540d44b4dd54bd02f7931b98887720aefea753
Vasiliy <vasiliy-ufa@mail.ru>
2019-12-23 21:37:55 +0500

    сделал ДЗ

--- 1.js
+++ 1.js
@@ -1,4 +1,21 @@
 'use strict';
-let arr = ["piter", "vasek", "banan"];
-console.log(arr[1]);
-console.log(arr[0]);
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    b1 = prompt('Во сколько обойдется?'),
+    a2 = prompt('Введите обязательную статью расходов в этом месяце'),
+    b2 = prompt('Во сколько обойдется?');
+appData.expenses.a1 = b1;
+appData.expenses.a2 = b2;
+
+console.log(appData);
+alert('бюджет на 1 день ' + appData.budget / 30 + ' руб.');
